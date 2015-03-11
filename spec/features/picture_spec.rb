e5b483b8c7d19bb95d9f6aec9b4246dc8662fe95
BobRazoswki <bobongithub@gmail.com>
2015-03-11 23:36:41 +0100

    paperclip work

--- spec/features/picture_spec.rb
+++ spec/features/picture_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe '' do
 	context '' do
-		before do
-			@document = Document.create(ref: 'bob', status: 'pending', pic:'')
-		end
 		it '' do
 			visit('/documents')
-			find(".img-bob")
-			expect(page).to have_content('')
+			click_link 'Add'
+			fill_in('Ref', :with => "bob")
+			attach_file 'Pic', Rails.root.join('spec/images/Bob_razowski-1.jpg')
+			click_button 'Upload Doc'
+			save_and_open_page
+			expect(page).to have_css('.img_bob')
 		end
 	end
 end

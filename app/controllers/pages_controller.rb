de6cfcd3fc9f40be7d66954d071433a813f8360b
BobRazoswki <bobongithub@gmail.com>
2015-03-21 17:30:59 +0100

    refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,29 +3,5 @@ class PagesController < ApplicationController
 	def home
 	end
 
-	def index
-		@documents = Document.all
-		redirect_to '/documents'
-	end
-
-	def show
-		@documents = Document.all
-		redirect_to '/documents'
-	end
-
-	def new
-		@new_document = Document.new
-		redirect_to '/documents'
-	end
-
-	def create
-    @new_document = Document.create(_document_params)
-    @new_document.save
-    redirect_to '/documents'
-  end
-
-	def update
-
-	end
 
 end

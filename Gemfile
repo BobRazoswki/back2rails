0755e3c0ec5c6143c6683dc52fdd386a2f53dc25
BobRazoswki <bobongithub@gmail.com>
2015-03-21 16:02:23 +0100

    back to normal

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem "paperclip", "~> 4.2"
-gem "ember-cli-rails"
 
 group :development, :test do
 	gem 'capybara'
--- spec/features/toggle-bar_spec.rb
+++ spec/features/toggle-bar_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 describe '' do
-	it 'open toggle bar', js: :true do
+	it 'open toggle bar once', js: :true do
 		visit('/')
 		click_link('sidebar-toggle')
-		save_and_open_page
-		expect(page).to have_css('open-sidebar')
+		expect(page).to have_css('.open-sidebar')
 	end
+
+	xit 'open toggle bar twice', js: :true do
+		visit('/')
+		click_link('sidebar-toggle')
+		expect(page).to have_css('.open-sidebar')
+		click_link('Documents')
+		expect(page).to have_content('Documents repo')
+		click_link('sidebar-toggle')
+		expect(page).to have_css('.open-sidebar')
+	end
+
 end
